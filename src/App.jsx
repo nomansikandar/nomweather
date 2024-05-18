06031459869e2fa979c630032c1692a1df795bc1
NOM <88772754+nomansikandar@users.noreply.github.com>
2024-05-18 12:38:25 +0500

    create left div

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import logo from "./assets/logo.png"
+import clouds from './assets/clouds.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mainContainer">
+      <div className="container">
+        <div className="leftDiv">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="weatherData">
+            <h1>16°</h1>
+            <div className="cityDateTime">
+              <h2>London</h2>
+              <p>06:09 - Monday, 9 Sep ‘23</p>
+            </div>
+            <img src={clouds} alt="" />
+          </div>
+        </div>
+        <div className="rightDiv">
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+      
     </>
   )
 }
