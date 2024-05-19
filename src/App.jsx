3ca10a63be19e77bbbe4a2b11c23c98ad4d59db6
NOM <88772754+nomansikandar@users.noreply.github.com>
2024-05-19 11:18:56 +0500

    left div css complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,28 @@ function App() {
 
   return (
     <>
-    <div className="mainContainer">
-      <div className="container">
-        <div className="leftDiv">
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="weatherData">
-            <h1>16°</h1>
-            <div className="cityDateTime">
-              <h2>London</h2>
-              <p>06:09 - Monday, 9 Sep ‘23</p>
+      <div className="mainContainer">
+        <div className="container">
+          <div className="content">
+          <div className="leftDiv">
+            <div className="logo">
+              <img src={logo} alt="" />
             </div>
-            <img src={clouds} alt="" />
+            <div className="weatherData">
+              <h1>16°</h1>
+              <div className="cityDateTime">
+                <h2>London</h2>
+                <p>06:09 - Monday, 9 Sep ‘23</p>
+              </div>
+              <img src={clouds} alt="" />
+            </div>
+          </div>
+          <div className="rightDiv">
+            
+          </div>
           </div>
         </div>
-        <div className="rightDiv">
-
-        </div>
-      </div>
-    </div>
-      
+      </div>     
     </>
   )
 }
