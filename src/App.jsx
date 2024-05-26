fb9fc9cb7e52fbceb4b7562217792c02646d1c35
NOM <88772754+nomansikandar@users.noreply.github.com>
2024-05-26 22:08:33 +0500

    right div content added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import logo from "./assets/logo.png"
 import clouds from './assets/clouds.png'
+import { FaTemperatureHigh } from "react-icons/fa";
+import { FaTemperatureLow } from "react-icons/fa";
+import { FaRegSnowflake } from "react-icons/fa";
+
+
 
 function App() {
 
@@ -23,7 +28,62 @@ function App() {
             </div>
           </div>
           <div className="rightDiv">
-            
+            <div className='searchBar'>
+              <input type="text" />
+            </div>
+            <p>Weather Details..</p>
+            <div className="tempDetails">
+              <h4>thunderstorm with light drizzle</h4>
+              <div className="tempMax">
+                <p>Temp max</p>
+                <p>19°</p>
+                <FaTemperatureHigh />                
+              </div>
+              <div className="tempMin">
+                <p>Temp min</p>
+                <p>15°</p>
+                <FaTemperatureLow />               
+              </div>
+              <div className="tempMin">
+                <p>Temp min</p>
+                <p>15°</p>
+                <FaTemperatureLow />               
+              </div>
+              <div className="tempMin">
+                <p>Temp min</p>
+                <p>15°</p>
+                <FaTemperatureLow />               
+              </div>
+              <div className="tempMin">
+                <p>Temp min</p>
+                <p>15°</p>
+                <FaTemperatureLow />               
+              </div>
+            </div>
+            <hr></hr>
+            <div className="todayWeather">
+              <h4>Today’s Weather Forecast...</h4>
+              <div className="hourlyTemp">
+                <FaRegSnowflake />
+                <div className="">
+                  <p>09:00</p>
+                  <p>Snow</p>
+                </div>
+                <div className="hourlyTempC">
+                  <p>19°</p>
+                </div>
+              </div>
+              <div className="hourlyTemp">
+                <FaRegSnowflake />
+                <div className="">
+                  <p>09:00</p>
+                  <p>Snow</p>
+                </div>
+                <div className="hourlyTempC">
+                  <p>19°</p>
+                </div>
+              </div>
+            </div>
           </div>
           </div>
         </div>
