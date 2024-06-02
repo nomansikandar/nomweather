5614c26993a470cc154e582efb6a979ef901684e
NOM <88772754+nomansikandar@users.noreply.github.com>
2024-06-02 19:05:32 +0500

    CSS FOR DESKTOP COMPLETED

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import logo from "./assets/logo.png"
 import clouds from './assets/clouds.png'
 import { FaTemperatureHigh } from "react-icons/fa";
 import { FaTemperatureLow } from "react-icons/fa";
-import { FaRegSnowflake } from "react-icons/fa";
+// import { FaRegSnowflake } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { MdOutlineWaterDrop } from "react-icons/md";
+import { BsClouds } from "react-icons/bs";
+import { FaWind } from "react-icons/fa";
 
 
 
@@ -30,38 +34,41 @@ function App() {
             <div className="rightDiv">
               <div className='searchBar'>
                 <input type="text" placeholder='Search Location...' />
+                <FaMagnifyingGlass className='magnifier' />
               </div>
+              <hr />
               <p className='weatherDetails'>Weather Details..</p>
               <div className="tempDetails">
                 <h4>thunderstorm with light drizzle</h4>
                 <div className="tempMax">
-                  <p>Temp max</p>
-                  <p>19°</p>
-                  <FaTemperatureHigh />                
+                  <p className='tempMaxText'>Temp max</p>
+                  <p className='tempMaxDeg'>19°</p>
+                  <FaTemperatureHigh className='tempMaxIcon' />                
                 </div>
                 <div className="tempMin">
-                  <p>Temp min</p>
-                  <p>15°</p>
-                  <FaTemperatureLow />               
+                  <p className='tempMinText'>Temp min</p>
+                  <p className='tempMinDeg'>15°</p>
+                  <FaTemperatureLow className='tempMinIcon' />               
                 </div>
                 <div className="tempMin">
-                  <p>Temp min</p>
-                  <p>15°</p>
-                  <FaTemperatureLow />               
+                  <p className='tempMinText' >Humadity</p>
+                  <p className='tempMinDeg'>58%</p>
+                  <MdOutlineWaterDrop className='tempMinIcons' />               
                 </div>
                 <div className="tempMin">
-                  <p>Temp min</p>
-                  <p>15°</p>
-                  <FaTemperatureLow />               
+                  <p className='tempMinText'>Cloudy</p>
+                  <p className='tempMinDeg'>86%</p>
+                  <BsClouds className='tempMinIcons' />               
                 </div>
                 <div className="tempMin">
-                  <p>Temp min</p>
-                  <p>15°</p>
-                  <FaTemperatureLow />               
+                  <p className='tempMinText'>Wind</p>
+                  <p className='tempMinWind'>5km/h</p>
+                  <FaWind className='tempMinIcons'/>               
                 </div>
               </div>
               <hr></hr>
-              <div className="todayWeather">
+              
+              {/* <div className="todayWeather">
                 <h4>Today’s Weather Forecast...</h4>
                 <div className="hourlyTemp">
                   <FaRegSnowflake />
@@ -83,7 +90,7 @@ function App() {
                     <p>19°</p>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
